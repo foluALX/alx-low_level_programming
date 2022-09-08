b266fbab640d03bb1a5264e4ba71b2c0d9086d8a
foluALX <laniyanfolu@gmail.com>
2022-09-08 16:02:27 -0700

    a script that prints with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main: entry point
+ * Description: c program that prints with printf
+ * Return: always return 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -3,3 +3,4 @@ a script that compiles but doesn't link
 script thatassembles code and save it in an output file
 a script that compiles a c file and creates executable
  a c file that prints using the function puts
+a function that prints with printf
