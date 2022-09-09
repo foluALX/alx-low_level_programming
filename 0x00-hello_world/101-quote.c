b7fefcfe9184119cbb3bad3a1c2690644f49321f
foluALX <laniyanfolu@gmail.com>
2022-09-08 17:13:21 -0700

    c programme that prints to the standard error - ans fix error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
 /**
  * main - entry point
  * Description: print a code to the standard error
- * Return: returns 1 
+ * Return: return 1
 */
+
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
