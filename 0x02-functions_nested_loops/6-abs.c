d0d1db372c53610eb1d9b2328a9845695ff25413
foluALX <laniyanfolu@gmail.com>
2022-09-13 16:28:35 -0700

    a function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @n: argument for function
+ * Return: returns the absolute value of n
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		/* turn -ve number to +ve */
+		n = (-1) * n;
+	}
+
+	return (n);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,3 +4,4 @@ a program to print a-z ten times
 a function that checks for lowercase character
 a function that checks for alphabetic character
 a function that prints the sign of a number
+a function that computes the absolute value of an integer
