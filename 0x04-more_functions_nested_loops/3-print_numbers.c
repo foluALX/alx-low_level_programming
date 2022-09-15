4f7edc1a3be5874ba3d8d6c6153600192a5555fd
foluALX <laniyanfolu@gmail.com>
2022-09-15 12:55:50 -0700

    a function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers,
+ * from 0 to 9
+ * Return: print to stdout the alphabet
+*/
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 a function that checks for uppercase character
  a function that checks for a digit
 a function that multiplies two integers
+a function that prints the numbers, from 0 to 9
