43a618ddac1fb8c8a049de1405ee747c43e98a45
foluALX <laniyanfolu@gmail.com>
2022-09-15 16:30:55 -0700

    print integer -error fixing

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,8 +10,8 @@ void print_number(int n)
 	/*check if n is negative*/
 	if (n < 0)
 	{
-		n = -n;
 		_putchar('-');
+		n = -n;
 	}
 
 	if (n == 0)
@@ -20,7 +20,7 @@ void print_number(int n)
 	}
 
 	/*print the first few digits*/
-	if ((n / 10) > 0)
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
