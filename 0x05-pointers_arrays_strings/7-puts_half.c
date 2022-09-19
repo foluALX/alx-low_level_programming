0bfe9f1a03c0dcd44b0921fec095fc2c7db0e252
foluALX <laniyanfolu@gmail.com>
2022-09-19 15:39:35 -0700

    a function that prints half of a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strlen - get length of string
+ * @s: pointer to char
+ * Return: length of str
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * puts_half - prints half of a string
+ * @str: a pointer to string
+ * Return: nothing
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	int size = _strlen(str);
+
+	if (size % 2 != 0)
+	{
+		i = (size / 2) + 1;
+	}
+	else
+	{
+		i = size / 2;
+	}
+
+	while (i < size)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -5,3 +5,4 @@ prints a string
 print string in reverse
 reverses a string
 a function that prints every other character of a string, starting with the first character, followed by a new line
+a function that prints half of a string, followed by a new line
