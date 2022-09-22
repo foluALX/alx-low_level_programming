875ab454c97800e9b7bb5c330cfa08283ec36158
foluALX <laniyanfolu@gmail.com>
2022-09-22 11:18:17 -0700

    a function that prints a buffer fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -27,7 +27,7 @@ void print_line(char *c, int s, int l)
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
-			putchar(',');
+			putchar('.');
 	}
 }
 
@@ -48,7 +48,7 @@ void print_buffer(char *b, int size)
 		if (i < size / 10)
 			print_line(b, 9, i);
 		else
-			print_line(b, size % 10 - 1, i);
+			print_line(b, (size % 10) - 1, i);
 
 		putchar('\n');
 	}
