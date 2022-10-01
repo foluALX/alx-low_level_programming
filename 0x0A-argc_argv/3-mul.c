fa1c72b744b4127c4c11207a31a5a89466c7d6e4
foluALX <laniyanfolu@gmail.com>
2022-09-30 18:17:42 -0700

    a program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the multpilication of 2 integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if success, else 1
+*/
+
+int main(int argc, char *argv[])
+{
+	int n, m, diff;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	mult = n * m;
+
+	printf("%qi\n", diff);
+
+	return (0);
+}
