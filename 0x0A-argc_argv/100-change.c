b7ae844b734d7436fb9545bfbb189d04fac2dd01
foluALX <laniyanfolu@gmail.com>
2022-09-30 18:54:55 -0700

    a program that prints the minimum number of coins to make change for an amount of money - error

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,6 +21,14 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
+	total = atoi(argv[1]); /*convert str to int*/
+
+	if (total <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
 	while (coins[position] != '\0')
 	{
 		if (total >= coins[position])
